1586defbaf718ef8c77a1b372e16e3f1db293ca7
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 16:58:20 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pulseedge/main.go
@@ -0,0 +1,23 @@
+// cmd/pulseedge/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"pulseedge/internal/pulseedge"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := pulseedge.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
